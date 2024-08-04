5bf94b1d4c472e63a8960a9144fe0089720c59b3
Thiago Amaral <amaral.thi@live.com>
2024-08-03 21:52:56 -0300

    botao reiniciar

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function sortear() {
     let sorteados = [];
 
     for (let i = 0; i < quantidade; i++) {
-        numero = obterNumerosAleatorios(de, ate);
+        let numero = obterNumerosAleatorios(de, ate);
 
         while(sorteados.includes(numero)){
             numero = obterNumerosAleatorios(de, ate);
@@ -16,10 +16,31 @@ function sortear() {
     }
 
     let resultado = document.getElementById("resultado");
-        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`;
-
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`;
+    alterarStatusBotao();
 }
 
 function obterNumerosAleatorios(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+    alterarStatusBotao();
 }
